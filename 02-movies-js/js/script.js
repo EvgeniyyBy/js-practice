fea6e6221769174a821437b0de462ef02ec2f302
evgeniyby <batname@mail.ru>
2022-08-01 15:48:57 +0300

    пример работы с циклом while

--- 02-movies-js/js/script.js
+++ 02-movies-js/js/script.js
@@ -8,9 +8,29 @@ const personalMovieDB = {
     privat: false
 };
 
-for (i = 0; i < 2; i++) {
+
+// 1.Работа с циклом for
+
+// for (i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = +prompt('На сколько оцените его?', '')
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+// 2.Работа с циклом while
+
+let i = 0;
+while (i < 2) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
     b = +prompt('На сколько оцените его?', '')
+    i++;
 
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
